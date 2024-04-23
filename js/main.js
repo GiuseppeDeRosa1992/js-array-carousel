c720982446c7797a943c1be631581b554ef13d13
GiuseppeDeRosa1992 <derosagiuseppe1092@gmail.com>
2024-04-23 17:18:20 +0200

    added button previous with .addEventListener "click" for go to back and forth img

--- js/main.js
+++ js/main.js
@@ -27,3 +27,25 @@ for (let i = 0; i < imgList.length; i++) {
 imgList[imgBlock+1].classList.add("block")
 console.log("Chi ha adesso display block?", imgBlock + 1)
 })
+
+
+//Richiamo tramite id il bottone dell'html
+document.getElementById("previous").addEventListener("click", function() {
+    //Inizio ciclo
+for (let i = 0; i < imgList.length; i++) {
+    //creo la costante img e gli dico che è uguale al numero del ciclo che in quel momento ha imgList
+    const img = imgList[i]
+    console.log(i, img)
+
+    //se img contiene block allora glielo rimuovo
+    if(img.classList.contains("block")) {
+        img.classList.remove("block");
+        console.log("ho tolto block all'immagine?", i);
+        //do alla variabile imgBlock lo stesso valore di i senno non viene visto fuori dal ciclo questo valore
+        imgBlock = i;
+    }
+}
+//per il tasto previuos cambio il +1 con -1 per andare avanti con next e andare indietro con previous
+imgList[imgBlock-1].classList.add("block")
+console.log("Chi ha adesso display block?", imgBlock - 1)
+})
