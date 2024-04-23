46e5cda9e55c628dffdcbbb003f1d7377766d734
GiuseppeDeRosa1992 <derosagiuseppe1092@gmail.com>
2024-04-23 15:51:21 +0200

    added class .image inside tag img html and added variable let imgList with console.log inside js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-console.log("ciao giuseppe")
+console.log("ciao giuseppe");
+
+//Prendo la lista di immagini da html con .getelementbyclassname
+let imgList = document.getElementsByClassName("image");
+console.log(imgList)
+
+//Inizio ciclo
+
